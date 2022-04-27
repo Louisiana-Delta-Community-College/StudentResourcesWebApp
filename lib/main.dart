00af5c588f278dca71f54f072acc740bb35129db
= <jaredfields@ladelta.edu>
2022-04-27 11:12:53 -0500

    Adds a 4K breakpoint directive to the ResponsiveWrapper Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'Flutter Modular Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
       builder: (context, widget) => ResponsiveWrapper.builder(
         ClampingScrollWrapper.builder(context, widget!),
         maxWidth: double.infinity,
@@ -39,14 +46,9 @@ class MyApp extends StatelessWidget {
           const ResponsiveBreakpoint.resize(480, name: MOBILE),
           const ResponsiveBreakpoint.autoScale(800, name: TABLET),
           const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+          const ResponsiveBreakpoint.autoScale(2460, name: '4K'),
         ],
       ),
-      title: 'Flutter Modular Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      routeInformationParser: Modular.routeInformationParser,
-      routerDelegate: Modular.routerDelegate,
     );
     // OR WITHOUT RESPONSIVE WRAPPER
     // return MaterialApp.router(
