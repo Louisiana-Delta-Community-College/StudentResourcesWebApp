8b780b72b9f44e9c737aa05d4cf25cc17e44673f
= <jaredfields@ladelta.edu>
2022-05-05 13:37:23 -0500

    Adds a wildcard "not found" page.

---
+++ lib/Pages/not_found/not_found_v.dart
@@ -0,0 +1,10 @@
+import 'package:schedule/common/common.dart';
+
+class NotFoundPage extends StatelessWidget {
+  const NotFoundPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("not found")));
+  }
+}
--- lib/Pages/pages.dart
+++ lib/Pages/pages.dart
@@ -1,3 +1,4 @@
 export 'home/home_v.dart';
 export 'schedule/schedule_v.dart';
+export 'not_found/not_found_v.dart';
 export 'other.dart';
